e46b2dc5ed09f7c795e123bffa38adf04c258a0f
Criss16 <cristinahatnean16@gmail.com>
2025-01-05 19:31:45 +0200

    Tutorial code

---
+++ service1/main.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const jwt = require('jsonwebtoken');
+const app = express()
+const port = 81
+
+
+app.use(express.json())
+
+app.post('/service1', async (req, res) => {
+    const { token } = req.body;
+    try{
+        const verifToken = jwt.verify(token, process.env.JWT_KEY)
+        if (verifToken) {
+            res.status(200).json({ message: 'Authenticated user accessed service1' });
+        }
+    } catch(err) {
+        console.error(err);
+        res.status(401).json({ message: 'User not authenticated' });
+    }
+
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
